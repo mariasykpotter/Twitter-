74980a0551f9255b510acae82ba626c6e41b3769
mariasykpotter <43301075+mariasykpotter@users.noreply.github.com>
2019-02-18 23:40:47 +0200

    Update twitter2.py

--- twitter2.py
+++ twitter2.py
@@ -23,6 +23,23 @@ def get_twit_json_values():
     """
     :param key: enter custom quantity of keys from twitts.json file
     :return: new list of dicts contains introduced above keys and their values
+    >>> get_twit_json_values()
+    Enter please your account:atkaterunner
+    {users|next_cursor|next_cursor_str|previous_cursor|previous_cursor_str|total_count}
+    users
+    [element_0|element_1|element_2|element_3|element_4|element_5|element_6|element_7|element_8|element_9|element_10|element_11|element_12|element_13|element_14|element_15|element_16|element_17|element_18|element_19]
+    0
+    {id|id_str|name|screen_name|location|description|url|entities|protected|followers_count|friends_count|listed_count|created_at|favourites_count|utc_offset|time_zone|geo_enabled|verified|statuses_count|lang|status|contributors_enabled|is_translator|is_translation_enabled|profile_background_color|profile_background_image_url|profile_background_image_url_https|profile_background_tile|profile_image_url|profile_image_url_https|profile_banner_url|profile_link_color|profile_sidebar_border_color|profile_sidebar_fill_color|profile_text_color|profile_use_background_image|has_extended_profile|default_profile|default_profile_image|following|live_following|follow_request_sent|notifications|muting|blocking|blocked_by|translator_type}
+    entities
+    {url|description}
+    url
+    {urls}
+    urls
+    [element_0]
+    0
+    {url|expanded_url|display_url|indices}
+    url
+    https://t.co/fBSzOW6fRe
     """
     with open("data/twitts.json", "r") as tj:
         data = json.loads(tj.read())
