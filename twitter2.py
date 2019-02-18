74298c966b0ebcbb3e795956abe1886ff804ed1e
mariasykpotter <43301075+mariasykpotter@users.noreply.github.com>
2019-02-18 23:25:02 +0200

    Update twitter2.py

--- twitter2.py
+++ twitter2.py
@@ -1,13 +1,15 @@
 import urllib.request, urllib.parse, urllib.error
-import twurl
 import json
+import twurl
 from geopy import Nominatim
 from os.path import exists
-
+from copy import deepcopy
 TWITTER_URL = 'https://api.twitter.com/1.1/friends/list.json'
+history = []
+history = []
 
 
-def save_twit_json(acct='Mariann01001908'):
+def save_twit_json(acct):
     '''
     :param acct: your twitter @account without symbol @
     :return: None, but will save your followers twitts in twitts.json file in current directiory
@@ -17,51 +19,40 @@ def save_twit_json(acct='Mariann01001908'):
     with open("data/twitts.json", "wb") as tj:
         tj.write(page.read())
     return None
-
-
-def get_twit_json_values(*key):
-    '''
+def get_twit_json_values():
+    """
     :param key: enter custom quantity of keys from twitts.json file
     :return: new list of dicts contains introduced above keys and their values
-    '''
+    """
     with open("data/twitts.json", "r") as tj:
         data = json.loads(tj.read())
-        data = data['users']
-        new_list = list()
-        for i in data:
-            new_dict = dict()
-            new_list.append(new_dict)
-            for n in key:
-                new_dict[n] = i[n]
-        if 'location' in key:
-            nom = Nominatim()
-            for i in new_list:
-                n = nom.geocode(i['location'])
-                i['latitude'] = n.latitude
-                i['longitude'] = n.longitude
-            return new_list
-        else:
-            return new_list
+        while isinstance(data, dict) or isinstance(data, list):
+            printer(data)
+            next_value = input()
+            if next_value == '..':
+                if history:
+                    data = history.pop()
+            elif isinstance(data, dict):
+                history.append(deepcopy(data))
+                data = data[next_value]
+            elif isinstance(data, list):
+                history.append(deepcopy(data))
+                data = data[int(next_value)]
+        printer(data)
 
+def printer(data):
+    msg = str(data)
+    if isinstance(data, dict):
+        data = data.keys()
+        msg = '|'.join(data)
+        msg = '{' + msg + '}'
+    elif isinstance(data, list):
+        data = ['element_{}'.format(i) for i in range(len(data))]
+        msg = '|'.join(data)
+        msg = '[' + msg + ']'
+    print(msg)
 
 if __name__ == '__main__':
-
-    if exists("data/twitts.json"):
-        with open("data/twitts.json", "r") as tj:
-            data = json.loads(tj.read())
-            data = data['users']
-            list_keys = list(data[0].keys())
-            print(list_keys, '\n')
-    else:
-        save_twit_json()
-
-    while True:
-        x = input('Please chose and enter keys from the list above: ')
-        x = x.replace(',', ' ').split()
-        try:
-            for i in get_twit_json_values(*x):
-                print(i)
-        except:
-            print(
-                'Maybe you enter something wrong, please check yuorself and try again.')
-            continue
+    acount = input("Enter please your account:")
+    save_twit_json(acount)
+    get_twit_json_values()
